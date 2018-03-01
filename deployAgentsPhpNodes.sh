8831ed814f1f13c8f057a687dcfbe41201d76180
Dan McInteer <daniel@danandstorm.co.nz>
2018-03-02 08:19:07 +1300

    proxy settings

--- deployAgentsPhpNodes.sh
+++ deployAgentsPhpNodes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Deploy Machine Agent
-wget -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/InstallAppdMachineAgent.sh | sudo bash
+wget -e use_proxy=yes -e https_proxy=http://sngidc-dmz-wsa-1.cisco.com/ -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/InstallAppdMachineAgent.sh | sudo bash
 
 
 #Deploy PHP Agent
-wget -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/InstallAppDPHPAgent.sh | sudo bash
+wget -e use_proxy=yes -e https_proxy=http://sngidc-dmz-wsa-1.cisco.com/ -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/InstallAppDPHPAgent.sh | sudo bash
 
