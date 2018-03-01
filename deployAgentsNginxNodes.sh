98ae0cdfc097e7d018120bd6c2df7d87ef9f85c8
Dan McInteer <daniel@danandstorm.co.nz>
2018-03-02 07:49:57 +1300

    update

--- deployAgentsNginxNodes.sh
+++ deployAgentsNginxNodes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Deploy Machine Agent
-wget -e use_proxy=yes -e https_proxy=http://sngidc-dmz-wsa-1.cisco.com/ -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/c3-appd-machine-agent-sample.sh | bash
+wget -e use_proxy=yes -e https_proxy=http://sngidc-dmz-wsa-1.cisco.com/ -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/InstallAppdMachineAgent.sh | sudo bash
 
 
 #Deploy NGINX Agent
-wget -e use_proxy=yes -e https_proxy=http://sngidc-dmz-wsa-1.cisco.com/ -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/ | bash
+sudo wget -e use_proxy=yes -e https_proxy=http://sngidc-dmz-wsa-1.cisco.com/ -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/installAppsNginxAgent.sh | sudo bash
 
--- deployAgentsPhpNodes.sh
+++ deployAgentsPhpNodes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Deploy Machine Agent
-wget -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/c3-appd-machine-agent-sample.sh | bash
+wget -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/InstallAppdMachineAgent.sh | sudo bash
 
 
 #Deploy PHP Agent
-wget -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/InstallAppDPHPAgent.sh | bash
+wget -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/InstallAppDPHPAgent.sh | sudo bash
 
