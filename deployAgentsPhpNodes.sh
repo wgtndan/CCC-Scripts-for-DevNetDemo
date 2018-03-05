e852517b9fd370702847ffe47c2dcc2bcb0b0a8f
Dan McInteer <daniel@danandstorm.co.nz>
2018-03-05 21:45:18 +1100

    remove cached github references

--- deployAgentsPhpNodes.sh
+++ deployAgentsPhpNodes.sh
@@ -5,5 +5,4 @@ wget -e use_proxy=yes -e https_proxy=http://sngidc-dmz-wsa-1.cisco.com/ -O - htt
 
 
 #Deploy PHP Agent
-wget -e use_proxy=yes -e https_proxy=http://sngidc-dmz-wsa-1.cisco.com/ -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/InstallAppDPHPAgent.sh | sudo bash
-
+wget --no-cache -e use_proxy=yes -e https_proxy=http://sngidc-dmz-wsa-1.cisco.com/ -O - https://raw.githubusercontent.com/wgtndan/CCC-Scripts-for-DevNetDemo/master/InstallAppDPHPAgent.sh | sudo bash
